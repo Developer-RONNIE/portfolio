1c84d9ab04f9daef622c7cc794236f767e52eb17
Developer-Ronnie <ronnieworkspace@gmail.com>
2024-08-13 18:26:22 +0530

    Added Card animation & Layout

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import { BackgroundGradientAnimation } from "./GradientBg";
 
 export const BentoGrid = ({
   className,
@@ -58,25 +59,41 @@ export const BentoGridItem = ({
       }}
     >
 
-        <div className={`${id===6} && 'flex justify-center h-full'`}>
-            <div className="w-full h-full absolute">
-                {img && (
-                    <img
-                       src={img}
-                       alt={img}
-                       className={cn(imgClassName, 'object-cover, object-center')} 
-                    />
-                )}
+      <div className={`${id===6} && 'flex justify-center h-full'`}>
+        <div className="w-full h-full absolute">
+          {img && (
+            <img
+              src={img}
+              alt={img}
+              className={cn(imgClassName, 'object-cover, object-center')} 
+              />
+              )}
+        </div>
+        <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
+          {spareImg && (
+            <img
+              src={spareImg}
+              alt={spareImg}
+              className={'object-cover, object-center w-full h-full'}
+        />
+        )}
+        </div>
+        {id === 6 && (
+          <BackgroundGradientAnimation>
+            <div className="absolute z-50 flex items-center justify-center text-white font-bold"/>
+          </BackgroundGradientAnimation>
+        )}
+
+        <div className={cn(
+          titleClassName, 'group-hover/ bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
+          )}>
+              <div className="font-sans font-extralight text-[#c1c2c3] text-sm md:text-xs lg:text-base z-10">
+                {description}
+              </div>
+              <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
+                {title}
+              </div>
             </div>
-        </div>
-      
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
-          {title}
-        </div>
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-          {description}
-        </div>
       </div>
     </div>
   );
---
+++ components/ui/Grid.Globe.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Globe = () => {
+  return (
+    <div>Grid.Globe</div>
+  )
+}
+
+export default Globe
