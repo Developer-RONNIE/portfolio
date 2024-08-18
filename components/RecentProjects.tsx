5f416cb80722a3794d2431a1496a903c334cba85
Developer-Ronnie <ronnieworkspace@gmail.com>
2024-08-18 22:47:22 +0530

    Successful testing of 3d Cards for Projects

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,6 +1,7 @@
 import { projects } from '@/data'
 import React from 'react'
 import { PinContainer } from './ui/3d-pin'
+import { div } from 'three/webgpu'
 
 const RecentProjects = () => {
   return (
@@ -12,8 +13,35 @@ const RecentProjects = () => {
         <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
             {projects.map(({id, title, des, img, iconLists, link}) => (
                 <div key = {id} className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
-                    <PinContainer>
-                        {title}
+                    <PinContainer title={title} href={link}>
+                        <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10'>
+                            <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
+                                <img src="/bg.png" alt="bg-img" />
+                            </div>
+                            <img 
+                                src={img} 
+                                alt={title}
+                                className="z-10 absolute bottom-0"
+                            />
+                        </div>
+
+                        <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>
+                            {title}
+                        </h1>
+
+                        <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
+                            {des}
+                        </p>
+
+                        <div className="flex items-center justify-between mt-7 mb-3">
+                            <div className="flex items-center">
+                                {iconLists.map((icon, index) => (
+                                    <div key={icon} className="">
+                                        <img src={icon} alt="{icon}" className='p-2' />
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
                     </PinContainer>
                 </div>
             ))}
