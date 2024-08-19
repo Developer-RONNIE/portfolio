3f482731c154ee7d44b2ec30a7fc028a08870621
Developer-Ronnie <ronnieworkspace@gmail.com>
2024-08-19 16:49:18 +0530

    2nd successful testing of 3d cards

--- components/ui/RecentProjects.tsx
+++ components/ui/RecentProjects.tsx
@@ -1,5 +1,6 @@
 import { projects } from '@/data'
 import React from 'react'
+import { PinContainer } from './3d-pin'
 
 const RecentProjects = () => {
   return (
@@ -9,9 +10,11 @@ const RecentProjects = () => {
             <span className="text-purple">recent projects</span>
         </h1>
         <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
-            {projects.map((project) =>(
-                <div>
-                    {project.title}
+            {projects.map(({id, title, des, img, iconLists, link}) =>(
+                <div key={id} className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]">
+                    <PinContainer>
+                        {title}
+                    </PinContainer>
                 </div>
             ))}
         </div>
