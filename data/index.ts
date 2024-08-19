d323d8713ebfeafa64c9355d81b9ddae46633358
Developer-Ronnie <ronnieworkspace@gmail.com>
2024-08-19 16:36:38 +0530

    Finalised Profile Card in Bento Grid

--- data/index.ts
+++ data/index.ts
@@ -8,12 +8,12 @@ export const navItems = [
   export const gridItems = [
     {
       id: 1,
-      title: "I prioritize client collaboration, fostering open communication ",
+      title: "Ensuring Open, Effective client communication",
       description: "",
       className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
       imgClassName: "w-full h-full",
-      titleClassName: "justify-end",
-      img: "/b1.svg",
+      titleClassName: "justify-end text-mycolor-200",
+      img: "/rbk.svg",
       spareImg: "",
     },
     {
