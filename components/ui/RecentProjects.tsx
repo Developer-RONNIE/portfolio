d4c7d854fbe72c4477b7e82556c8d4dac6fb977d
Developer-Ronnie <ronnieworkspace@gmail.com>
2024-08-21 11:18:07 +0530

    Images & Tools for Projects Added to 3D project Cards

--- components/ui/RecentProjects.tsx
+++ components/ui/RecentProjects.tsx
@@ -23,6 +23,28 @@ const RecentProjects = () => {
                                 className="z-10 absolute bottom-0"
                             />
                         </div>
+
+                        <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                            {title}
+                        </h1>
+
+                        <p className="lg:text-xl lg:font-normal font-light text-sm line clamp-2">
+                            {des}
+                        </p>
+
+                        <div className="flex items-center jusitfy-between mt-7 mb-3">
+                            <div className="flex items-center">  
+                                {iconLists.map((icon, index) => (
+                                    <div key={icon} className='border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center'
+                                    style={{
+                                        transform: `translateX(-${5 * index * 2}px)`
+                                    }}>
+                                        <img src={icon} alt={icon} className="p-2"/>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+
                     </PinContainer>
                 </div>
             ))}
