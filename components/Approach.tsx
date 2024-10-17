bd6b8ffe3a2eeee97ce6b2a73790300741508c82
Developer-Ronnie <ronnieworkspace@gmail.com>
2024-10-17 05:43:43 +0530

    added approach data to the cards

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -12,13 +12,19 @@ const Approach = () => {
             <span className="text-purple"> approach</span>
         </h1>
       <div className="my-20 flex flex-col lg:flex-row items-center justify-center gap-4 ">
-        <Card title="Sheetal is Nisha" icon={<AceternityIcon />}>
+        <Card 
+            title="Planning & Strategy" 
+            icon={<AceternityIcon order="Phase 1"/>}
+            description = "We'll collaborate to map out your website's goals, target audience, and key functionalities. We'll discuss things like site structure, naviagtion, and content requirements.">
           <CanvasRevealEffect
             animationSpeed={5.1}
             containerClassName="bg-emerald-900"
           />
         </Card>
-        <Card title="Nisha is Munni" icon={<AceternityIcon />}>
+        <Card 
+            title="Development & Progress Update" 
+            icon={<AceternityIcon order="Phase 2"/>}
+            description = "Once we agree ont he plan, I cue my lofi playlist and dive into coding. From initial sketeches to poslished build, I keep you updated every step of the way. ">
           <CanvasRevealEffect
             animationSpeed={3}
             containerClassName="bg-black"
@@ -28,10 +34,11 @@ const Approach = () => {
             ]}
             dotSize={2}
           />
-          {/* Radial gradient for the cute fade */}
-          <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
         </Card>
-        <Card title="Munni is Aditi" icon={<AceternityIcon />}>
+        <Card 
+            title="Development & Launch" 
+            icon={<AceternityIcon order="Phase 3"/>}
+            description = "This is where the magic happens! Based on the approved design, I'll translate everything into functional code, building your website fromt he ground up.">
           <CanvasRevealEffect
             animationSpeed={3}
             containerClassName="bg-sky-600"
@@ -47,10 +54,12 @@ const Card = ({
   title,
   icon,
   children,
+  description
 }: {
   title: string;
   icon: React.ReactNode;
   children?: React.ReactNode;
+  description: string;
 }) => {
   const [hovered, setHovered] = React.useState(false);
   return (
@@ -80,33 +89,27 @@ const Card = ({
         <div className="text-center group-hover/canvas-card:-translate-y-4 group-hover/canvas-card:opacity-0 transition duration-200 w-full  mx-auto flex items-center justify-center">
           {icon}
         </div>
-        <h2 className="dark:text-white text-xl opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
+        <h2 className="dark:text-white text-3xl opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200 text-center">
           {title}
+        </h2>
+        <h2 className="text-sm dark:text-white  opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200 text-center " style={{color: '#e4ecff'}}>
+          {description}
         </h2>
       </div>
     </div>
   );
 };
 
-const AceternityIcon = () => {
+const AceternityIcon = ({order}: {order : string}) => {
   return (
-    <svg
-      width="66"
-      height="65"
-      viewBox="0 0 66 65"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-10 w-10 text-black dark:text-white group-hover/canvas-card:text-white "
-    >
-      <path
-        d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
-        stroke="currentColor"
-        strokeWidth="15"
-        strokeMiterlimit="3.86874"
-        strokeLinecap="round"
-        style={{ mixBlendMode: "darken" }}
-      />
-    </svg>
+    <div>
+        <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                {order}
+            </span>
+        </button>
+    </div>
   );
 };
 
