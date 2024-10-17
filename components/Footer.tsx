69060235dec1c4bed18cb7fc4bbf2b4b2366f3f1
Developer-Ronnie <ronnieworkspace@gmail.com>
2024-10-17 07:57:46 +0530

    Modified footer social media

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,20 +32,24 @@ const Footer = () => {
         </a>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+      <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((info) => (
+            <a
+            key={info.id}
+            href={info.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 border border-black-300 rounded-xl">
+              <img src={info.img} alt="icons" width={20} height={20} />
+            </a>
+          ))}
+        </div>
+        
         <p className="md:text-base text-sm md:font-normal font-light">
           Copyright © 2024 Alapan
         </p>
 
-        <div className="flex items-center md:gap-3 gap-6">
-          {socialMedia.map((info) => (
-            <div
-              key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200  border border-black-300 rounded-xl"
-            >
-              <img src={info.img} alt="icons" width={20} height={20} />
-            </div>
-          ))}
-        </div>
+        
       </div>
 
     </footer>
